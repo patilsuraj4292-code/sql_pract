9b304c554a00102e6b9d187eddcacc8bd3849174
patilsuraj4292-code <patilsuraj4292@gmail.com>
2026-01-06 22:25:07 +0900

    Update and rename sql_cities.sql to sql_queries.sql

--- sql_cities.sql
+++
@@ -1,8 +0,0 @@
---query to print top 5 cities with highest spends and their percentage contribution of total credit card spends
-with total_spent_cte as (
-select sum(cast(amount as bigint))as total_spent
-from credit_card_transcations)
-select top 5 city,sum(amount) as expense,total_spent,cast((sum(amount)*1.0 /total_spent) * 100 as decimal(5,2)) as percentage_contribution
-from credit_card_transcations,total_spent_cte
-group by city,total_spent
-order by expense desc
